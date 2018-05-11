f79226d5f177dd01e607c567f03ce4b47d20a3f1
Alan Luo <aluo64@gmail.com>
2018-05-11 18:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(kittens,Broom) {
-  return ["Broom", ...kittens]
+  return [Broom, ...kittens]
 }
 
 function prependKitten(Arnold) {
-  return [...kittens, "Arnold"]
+  return [...kittens, Arnold]
 }
