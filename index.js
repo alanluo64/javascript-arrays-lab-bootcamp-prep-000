077bdbf7b296fb57f5fcc267a975dc8ece7bf9da
Alan Luo <aluo64@gmail.com>
2018-05-11 18:27:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function removeLastKitten(kittens) {
   return kittens.slice(2, kittens.length -1)
 }
 
-function removeFirstKitten(kittens){
-  return kittens.slice(0, kittens.length -1)
+function removeFirstKitten(){
+  return kittens.slice(1)
 }
